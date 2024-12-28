d4bdb530f135da2924ea8be7bd7588c255724a5c
Nitin Gupta <nitin@rocketium.com>
2024-12-28 19:21:36 +0530

    feat: added auth config, schema validation and session for user

---
+++ src/schemas.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const signUpSchema = z.object({
+	email: z.string({ required_error: 'Email is required' }).email('Invalid email'),
+	password: z
+		.string({ required_error: 'Password is required' })
+		.min(8, 'Password must be at least 8 characters')
+		.max(32, 'Password must be at most 32 characters'),
+});
+
+export const signInSchema = z.object({
+	email: z.string({ required_error: 'Email is required' }).email('Invalid email'),
+	password: z.string({ required_error: 'Password is required' }),
+});
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,8 +1,9 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-
-import { db } from "~/server/db";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import bcrypt from 'bcryptjs';
+import { type DefaultSession, type NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { signInSchema } from '~/schemas';
+import { db } from '~/server/db';
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -10,19 +11,19 @@ import { db } from "~/server/db";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
-  }
+declare module 'next-auth' {
+	interface Session extends DefaultSession {
+		user: {
+			id: string;
+			// ...other properties
+			// role: UserRole;
+		} & DefaultSession['user'];
+	}
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
+	// interface User {
+	//   // ...other properties
+	//   // role: UserRole;
+	// }
 }
 
 /**
@@ -31,25 +32,57 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authConfig = {
-  providers: [
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
-  adapter: PrismaAdapter(db),
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-  },
+	providers: [
+		Credentials({
+			name: 'Credentials',
+			credentials: {
+				email: { label: 'Email', type: 'text', placeholder: 'jsmith@gmail.com' },
+				password: { label: 'Password', type: 'password' },
+			},
+			authorize: async credentials => {
+				try {
+					const { email, password } = await signInSchema.parseAsync(credentials);
+					const user = await db.user.findUnique({
+						where: { email },
+					});
+					if (!user) {
+						throw new Error('User not found');
+					}
+					if (!user.password) {
+						throw new Error('Invalid password');
+					}
+					const passWordMatch = await bcrypt.compare(password, user.password);
+					if (!passWordMatch) {
+						throw new Error('Invalid password');
+					}
+					return user;
+				} catch (error) {
+					console.error(error);
+					return null;
+				}
+			},
+		}),
+		/**
+		 * ...add more providers here.
+		 *
+		 * Most other providers require a bit more work than the Discord provider. For example, the
+		 * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+		 * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+		 *
+		 * @see https://next-auth.js.org/providers/github
+		 */
+	],
+	session: {
+		strategy: 'jwt',
+	},
+	adapter: PrismaAdapter(db),
+	callbacks: {
+		session: ({ session, token }) => ({
+			...session,
+			user: {
+				...session.user,
+				id: token.sub,
+			},
+		}),
+	},
 } satisfies NextAuthConfig;
