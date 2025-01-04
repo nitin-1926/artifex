de24e229696cdb29153d5270811cf09bc56e9f77
Nitin Gupta <nitin@rocketium.com>
2025-01-05 01:48:00 +0530

    feat: created dashboard content

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,5 @@
-'use client';
+import { DashboardContent } from '~/components/dashboard/DashboardContent';
 
-import { logout } from '../actions/auth';
-
-const DashboardPage = () => {
-	return (
-		<div>
-			<p>Dashboard</p>
-			<button onClick={logout}>Logout</button>
-		</div>
-	);
-};
-
-export default DashboardPage;
+export default function DashboardPage() {
+	return <DashboardContent />;
+}
