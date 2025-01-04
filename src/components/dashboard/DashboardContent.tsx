569ec82d5effe61c40f3f97bd122286b86478d64
Nitin Gupta <nitin@rocketium.com>
2025-01-05 02:02:01 +0530

    feat: added ability to create a new room

--- src/components/dashboard/DashboardContent.tsx
+++ src/components/dashboard/DashboardContent.tsx
@@ -1,6 +1,7 @@
 import UserMenu from '~/components/dashboard/UserMenu';
 import { auth } from '~/server/auth';
 import { db } from '~/server/db';
+import CreateRoom from './CreateRoom';
 
 export async function DashboardContent() {
 	const session = await auth();
@@ -23,6 +24,14 @@ export async function DashboardContent() {
 		<div className="flex h-screen w-full">
 			<div className="flex h-screen min-w-[264px] flex-col border-r border-gray-200 bg-white p-2">
 				<UserMenu email={user.email} />
+			</div>
+			<div className="flex h-screen w-full flex-col">
+				<div className="flex min-h-[50px] items-center border-b border-gray-200 bg-white pl-8">
+					<h2 className="text-[13px]">Recents</h2>
+				</div>
+				<div className="flex h-full flex-col gap-10 p-8">
+					<CreateRoom />
+				</div>
 			</div>
 		</div>
 	);
