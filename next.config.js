44f8344bb14db585909b69fcc116a83c3d8a4993
Nitin Gupta <nitin@rocketium.com>
2025-01-05 03:17:37 +0530

    fix: handled redirection on home route

--- next.config.js
+++ next.config.js
@@ -2,9 +2,19 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import './src/env.js';
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/dashboard',
+				permanent: true,
+			},
+		];
+	},
+};
 
 export default config;
