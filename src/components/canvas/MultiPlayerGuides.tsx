3703558c08b1553e973460db3ce57c412b599430
Nitin Gupta <nitin@rocketium.com>
2025-01-05 01:02:54 +0530

    feat: added real time drawing rendering for multi users

--- src/components/canvas/MultiPlayerGuides.tsx
+++ src/components/canvas/MultiPlayerGuides.tsx
@@ -1,6 +1,8 @@
-import { useOthersConnectionIds } from '@liveblocks/react';
+import { shallow, useOthersConnectionIds, useOthersMapped } from '@liveblocks/react';
 import { memo } from 'react';
 import Cursor from './Cursor';
+import PathLayerComponent from './PathLayerComponent';
+import { rgbToHex } from '~/utils';
 
 const Cursors = memo(() => {
 	const ids = useOthersConnectionIds();
@@ -15,10 +17,43 @@ const Cursors = memo(() => {
 
 Cursors.displayName = 'Cursors';
 
+const Drafts = memo(() => {
+	const others = useOthersMapped(
+		other => ({
+			pencilDraft: other.presence.pencilDraft,
+			penColor: other.presence.penColor,
+		}),
+		shallow,
+	);
+
+	return (
+		<>
+			{others.map(([key, other]) => {
+				if (other.pencilDraft) {
+					return (
+						<PathLayerComponent
+							key={key}
+							x={0}
+							y={0}
+							points={other.pencilDraft}
+							fill={other.penColor ?? { r: 217, g: 217, b: 217 }}
+							opacity={100}
+						/>
+					);
+				}
+				return null;
+			})}
+		</>
+	);
+});
+
+Drafts.displayName = 'Drafts';
+
 const MultiplayerGuides = memo(() => {
 	return (
 		<>
 			<Cursors />
+			<Drafts />
 		</>
 	);
 });
