cd0a0fce66d1b410af558cfa558dca919f79c814
Nitin Gupta <nitin@rocketium.com>
2025-01-24 21:18:29 +0530

    fix: fixed linting errors

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -174,24 +174,24 @@ const CollisionMechanism = React.forwardRef<
 				ref={beamRef}
 				animate="animate"
 				initial={{
-					translateY: beamOptions.initialY || '-200px',
-					translateX: beamOptions.initialX || '0px',
-					rotate: beamOptions.rotate || 0,
+					translateY: beamOptions.initialY ?? '-200px',
+					translateX: beamOptions.initialX ?? '0px',
+					rotate: beamOptions.rotate ?? 0,
 				}}
 				variants={{
 					animate: {
-						translateY: beamOptions.translateY || '1800px',
-						translateX: beamOptions.translateX || '0px',
-						rotate: beamOptions.rotate || 0,
+						translateY: beamOptions.translateY ?? '1800px',
+						translateX: beamOptions.translateX ?? '0px',
+						rotate: beamOptions.rotate ?? 0,
 					},
 				}}
 				transition={{
-					duration: beamOptions.duration || 8,
+					duration: beamOptions.duration ?? 8,
 					repeat: Infinity,
 					repeatType: 'loop',
 					ease: 'linear',
-					delay: beamOptions.delay || 0,
-					repeatDelay: beamOptions.repeatDelay || 0,
+					delay: beamOptions.delay ?? 0,
+					repeatDelay: beamOptions.repeatDelay ?? 0,
 				}}
 				className={cn(
 					'absolute left-0 top-20 m-auto h-14 w-px rounded-full bg-gradient-to-t from-indigo-500 via-purple-500 to-transparent',
