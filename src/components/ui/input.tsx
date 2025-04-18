ebe235c3ad1208cbd83dda36257e5d95712cfa6e
Nitin Gupta <gupta7nitin@gmail.com>
2025-04-18 16:35:00 +0530

    refact: update input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-	const radius = 100; // change this to increase the radius of the hover effect
+	const radius = 130;
 	const [visible, setVisible] = React.useState(false);
 
 	const mouseX = useMotionValue(0);
