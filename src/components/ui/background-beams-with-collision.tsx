06c3600a8a54e1520c401967b3402142648cc5ae
Nitin Gupta <gupta7nitin@gmail.com>
2025-05-18 14:28:00 +0530

    refact: update background-beams component

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -70,8 +70,7 @@ export const BackgroundBeamsWithCollision = ({
 		<div
 			ref={parentRef}
 			className={cn(
-				'h-screen bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative flex items-center w-full justify-center overflow-hidden',
-				// h-screen if you want bigger
+				'relative flex min-h-screen w-full items-center justify-center overflow-hidden bg-transparent',
 				className,
 			)}
 		>
