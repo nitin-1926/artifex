babdac3b38d6db42f5e75e5a3bb65c631c3966e4
Nitin Gupta <gupta7nitin@gmail.com>
2025-05-23 17:28:00 +0530

    refact: update background-beams component

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -86,7 +86,7 @@ export const BackgroundBeamsWithCollision = ({
 			{children}
 			<div
 				ref={containerRef}
-				className="absolute bottom-0 bg-neutral-100 w-full inset-x-0 pointer-events-none"
+				className="pointer-events-none absolute inset-x-0 bottom-0 w-full bg-background/70"
 				style={{
 					boxShadow:
 						'0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset',
