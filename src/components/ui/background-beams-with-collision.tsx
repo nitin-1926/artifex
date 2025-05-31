0527bca0115373c6c4ea10d09ffe1a5f49a2fd30
Nitin Gupta <gupta7nitin@gmail.com>
2025-05-31 17:14:00 +0530

    refact: update background-beams component

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -232,7 +232,7 @@ const Explosion = ({ ...props }: React.HTMLProps<HTMLDivElement>) => {
 				animate={{ opacity: 1 }}
 				exit={{ opacity: 0 }}
 				transition={{ duration: 1.5, ease: 'easeOut' }}
-				className="absolute -inset-x-10 top-0 m-auto h-2 w-10 rounded-full bg-gradient-to-r from-transparent via-indigo-500 to-transparent blur-sm"
+				className="absolute -inset-x-10 top-0 m-auto h-2 w-10 rounded-full bg-gradient-to-r from-transparent via-primary to-transparent blur-sm"
 			></motion.div>
 			{spans.map(span => (
 				<motion.span
