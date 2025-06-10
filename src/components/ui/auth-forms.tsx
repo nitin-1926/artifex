603d871b518b73efdb0be695ad1bb3d2fdb58d21
Nitin Gupta <gupta7nitin@gmail.com>
2025-06-10 10:45:00 +0530

    feat: update auth forms

--- src/components/ui/auth-forms.tsx
+++ src/components/ui/auth-forms.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
 import { cn } from '~/lib/utils';
-import { TextGenerateEffect } from './text-generate-effect';
+import { ThemeToggle } from './theme-toggle';
 
 type AuthFormProps = {
 	type: 'signup' | 'signin';
