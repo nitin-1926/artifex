5317a993a01e4f178f153b673a73c7ba6956b2e8
Nitin Gupta <gupta7nitin@gmail.com>
2025-10-17 09:05:00 +0530

    refact: update ZoomOutButton styling

--- src/components/toolsbar/ZoomOutButton.tsx
+++ src/components/toolsbar/ZoomOutButton.tsx
@@ -3,8 +3,8 @@ import IconButton from './IconButton';
 
 const ZoomOutButton = ({ onClick, disabled }: { onClick: () => void; disabled?: boolean }) => {
 	return (
-		<IconButton onClick={onClick} disabled={disabled}>
-			<AiOutlineZoomOut size={22} color="#888" />
+		<IconButton onClick={onClick} disabled={disabled} label="Zoom out">
+			<AiOutlineZoomOut size={20} />
 		</IconButton>
 	);
 };
